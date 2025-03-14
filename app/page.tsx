d0f229873b504480ceb6ea363e495a4bed48d52f
NyiNyiZin-cmd <nyinyizin951@gmail.com>
2025-03-14 22:14:14 +0900

    redix ui setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@radix-ui/themes";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello Developer</h1>
+      <Button>New Issue</Button>
     </div>
   );
 }
