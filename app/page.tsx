5fb52fe2a2ce4d883561da75066c9292e183b762
NyiNyiZin-cmd <nyinyizin951@gmail.com>
2025-03-14 22:34:08 +0900

    Build the new issue page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { Button } from "@radix-ui/themes";
 export default function Home() {
   return (
     <div>
-      <h1>Hello Developer</h1>
       <Button>New Issue</Button>
     </div>
   );
